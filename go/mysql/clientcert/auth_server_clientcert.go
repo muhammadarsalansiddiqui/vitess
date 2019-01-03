cff45ef446d8022572c614121603930fb63d821b
tpetr <tpetr@hubspot.com>
2019-01-03 13:28:19 -0500

    support different auth methods + store VTGateCallerID ahead of time

--- go/mysql/clientcert/auth_server_clientcert.go
+++ go/mysql/clientcert/auth_server_clientcert.go
@@ -1,27 +1,38 @@
 package clientcert
 
 import (
+	"flag"
 	"fmt"
 	"net"
 	"vitess.io/vitess/go/mysql"
+	"vitess.io/vitess/go/vt/log"
 	querypb "vitess.io/vitess/go/vt/proto/query"
 )
 
-type AuthServerClientCert struct {}
+var clientcertAuthMethod = flag.String("mysql_clientcert_auth_method", mysql.MysqlClearPassword, "client-side authentication method to use. Supported values: mysql_clear_password, dialog.")
+
+type AuthServerClientCert struct {
+	Method string
+}
 
 type UserData struct {
-	conn *mysql.Conn
+	querypb.VTGateCallerID
 }
 
 // Init is public so it can be called from plugin_auth_ldap.go (go/cmd/vtgate)
 func Init() {
-	ascc := &AuthServerClientCert{}
+	if *clientcertAuthMethod != mysql.MysqlClearPassword && *clientcertAuthMethod != mysql.MysqlDialog {
+		log.Exitf("Invalid mysql_clientcert_auth_method value: only support mysql_clear_password or dialog")
+	}
+	ascc := &AuthServerClientCert{
+		Method: *clientcertAuthMethod,
+	}
 	mysql.RegisterAuthServerImpl("clientcert", ascc)
 }
 
 // AuthMethod is part of the AuthServer interface.
 func (ascc *AuthServerClientCert) AuthMethod(user string) (string, error) {
-	return "mysql_clear_password", nil
+	return ascc.Method, nil
 }
 
 // Salt is not used for this plugin.
@@ -46,14 +57,13 @@ func (ascc *AuthServerClientCert) Negotiate(c *mysql.Conn, user string, remoteAd
 	}
 
 	return &UserData{
-		conn: c,
+		VTGateCallerID: querypb.VTGateCallerID{
+			Username: certs[0].Subject.CommonName,
+			Groups:   certs[0].DNSNames,
+		},
 	}, nil
 }
 
 func (ud *UserData) Get() *querypb.VTGateCallerID {
-	certs := ud.conn.GetTLSClientCerts()
-	return &querypb.VTGateCallerID{
-		Username: certs[0].Subject.CommonName,
-		Groups: certs[0].DNSNames,
-	}
+	return &ud.VTGateCallerID
 }
